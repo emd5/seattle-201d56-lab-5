ad15ca827242d06d88d83c02e6ca96c7a9be955e
emd5 <elizabeth.mahoney.dao@gmail.com>
2019-03-29 21:51:47 -0700

    completed problem 3, test passed

--- lab-a/starter-code/app.js
+++ lab-a/starter-code/app.js
@@ -40,7 +40,7 @@ function multiply(a, b) { //eslint-disable-line
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -57,11 +57,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  let output = [];
+  let totalSum = sum(sum(a,b)[0], c)[0];
+  let totalMultiply = multiply(multiply(a,b)[0], c)[0];
+  let sumMessage = a + ' and ' + b + ' and ' + c +' sum to ' + totalSum + '.';
+  let multiplyMessage = 'The product of ' + a + ' and ' + b + ' and ' + c +' is ' + totalMultiply + '.';
 
+  output.push(totalSum, totalMultiply, sumMessage, multiplyMessage);
+
+  return output;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
